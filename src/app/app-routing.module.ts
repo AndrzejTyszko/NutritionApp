991e34486f3aaf6b8995e84676fd7dc8ad50d46b
Andrzej Tyszko <a.tyszko@medicalgorithmics.com>
2024-06-06 23:55:20 +0200

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'saved-data',
+    loadChildren: () => import('./saved-data/saved-data.module').then(m => m.SavedDataPageModule)
   },
   {
     path: '',
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NutritionService } from '../services/nutrition.service';
+import { DatabaseService } from '../services/database.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,46 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  query: string = '';
+  nutritionData: any;
+  date: string = ''; // Define the date property
 
-  constructor() {}
+  constructor(private nutritionService: NutritionService, private databaseService: DatabaseService) { }
 
+  getNutrition() {
+    this.nutritionService.getNutrition(this.query).subscribe(
+      data => {
+        this.nutritionData = data.items;
+      },
+      error => {
+        console.error('Error:', error);
+      }
+    );
+  }
+
+  saveData() {
+    if (this.date && this.nutritionData) {
+      this.databaseService.saveNutritionData(this.date, this.nutritionData);
+    }
+  }
+
+  loadData() {
+    if (this.date) {
+      this.databaseService.getNutritionData(this.date).then(data => {
+        this.nutritionData = data;
+      });
+    }
+  }
+
+  deleteData() {
+    if (this.date) {
+      this.databaseService.deleteNutritionData(this.date);
+      this.nutritionData = null;
+    }
+  }
+
+  getTotal(nutrient: string): number {
+    if (!this.nutritionData) return 0;
+    return this.nutritionData.reduce((total: any, item: { [x: string]: any; }) => total + item[nutrient], 0);
+  }
 }
